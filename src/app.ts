e649eb76a711d25743f5a1e4d7ae8a67f29af90b
Guiilherme Ennes <jaksonguilherme2@gmail.com>
2024-10-15 11:26:26 -0300

    Teste e2e de autenticação

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import fastify from 'fastify';
-import { appRoutes } from './http/routes';
-import { ZodError } from 'zod';
-import { env } from './env';
-import fastifyJwt from '@fastify/jwt';
+import fastify from "fastify";
+import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
 app.register(fastifyJwt, { secret: env.JWT_SECRET });
@@ -13,11 +13,11 @@ app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
-      .send({ message: 'Validation Error', issues: error.format() });
+      .send({ message: "Validation Error", issues: error.format() });
   }
 
-  if (env.NODE_ENV !== 'production') {
+  if (env.NODE_ENV !== "production") {
     console.error(error);
   }
-  return reply.status(500).send({ message: 'Internal Server Error' });
+  return reply.status(500).send({ message: "Internal Server Error" });
 });
