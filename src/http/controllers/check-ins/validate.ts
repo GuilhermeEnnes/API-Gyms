1a98d567475af54ef104f9ffa7716edf5d77ecbf
Guiilherme Ennes <jaksonguilherme2@gmail.com>
2024-10-16 12:11:26 -0300

    criação dos controlers das rotas de usuario, de academias e de checkins + reorganização das pastas.

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeValidateCheckInUseCase } from "@/use-cases/factories/make-validate-check-in-use-case";
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const validateCheckInParamsSchema = z.object({
+    checkInId: z.string().uuid(),
+  });
+  const { checkInId } = validateCheckInParamsSchema.parse(request.params);
+
+  const checkInUseCase = makeValidateCheckInUseCase();
+  await checkInUseCase.execute({
+    checkInId,
+  });
+
+  return reply.status(204).send();
+}
