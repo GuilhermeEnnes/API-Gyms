72b88a9b124c15310499ff8b0206d9a43bb10ece
Guiilherme Ennes <jaksonguilherme2@gmail.com>
2024-11-11 12:28:12 -0300

    Implementação da lógica de refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod';
-import { FastifyRequest, FastifyReply } from 'fastify';
-import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error';
-import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case';
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 
 export async function authenticate(
   request: FastifyRequest,
@@ -29,7 +29,24 @@ export async function authenticate(
         },
       },
     );
-    return reply.status(200).send({ token });
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      },
+    );
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message });
